5edaf8f1073757700cf437888eeac7e67624f6c0
Nikhila C <115739037+Nikhilaniki@users.noreply.github.com>
2023-12-21 19:42:05 +0700

    changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import LandingPage from "./components/landingPage";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from "./pages/Home";
 import CreatBook from "./pages/CreateBook";
 import DeleteBook from "./pages/DeleteBook";
@@ -9,14 +11,16 @@ import BackButton from "./components/BackButton";
 
 const App = () => {
   return (
-    <Routes>
-      <Route exact path="/" element={<Home />} />
-      <Route path="/books/create" element={<CreatBook />} />
-      <Route path="/books/details/:id" element={<ShowBook />} />
-      <Route path="/books/edit/:id" element={<EditBook />} />
-      <Route path="/books/delete/:id" element={<DeleteBook />} />
+    <LandingPage/>
+    // <Routes>
+    //   {
+    //   /* <Route exact path="/" element={<Home />} />
+    //   <Route path="/books/create" element={<CreatBook />} />
+    //   <Route path="/books/details/:id" element={<ShowBook />} />
+    //   <Route path="/books/edit/:id" element={<EditBook />} />
+    //   <Route path="/books/delete/:id" element={<DeleteBook />} /> */}
       
-    </Routes>
+    // </Routes>
   );
 };
 
