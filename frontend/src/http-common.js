9413f9022cc5bbe9f56d7158b47aecef744d3cfa
FuhadSanin <fuhadsanin500@gmail.com>
2024-01-21 13:31:23 +0530

    Live url

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export default axios.create({
-  baseURL: "http://localhost:10000",
+  baseURL: "https://mongodb-books-store.onrender.com",
   headers: {
     "Content-type": "application/json",
   },
